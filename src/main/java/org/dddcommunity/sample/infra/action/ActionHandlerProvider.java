aaa669ac3104c7c99f29ab6572139c121a427bf0
Joabe Costa <joabetc@hotmail.com>
2021-09-06 23:36:49 +0200

    Create base ActionHandlerProvider class

---
+++ src/main/java/org/dddcommunity/sample/infra/action/ActionHandlerProvider.java
@@ -0,0 +1,14 @@
+package org.dddcommunity.sample.infra.action;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+
+@RequiredArgsConstructor
+abstract class ActionHandlerProvider<H extends ActionHandler<?, ?>> {
+    private final ApplicationContext applicationContext;
+    private final Class<H> type;
+
+    public H get() {
+        return applicationContext.getBean(type);
+    }
+}
