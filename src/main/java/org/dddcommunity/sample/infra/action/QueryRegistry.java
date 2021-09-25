3fe99419b6a1dcc78612a0f95b8867fc071fc997
Joabe Costa <joabetc@hotmail.com>
2021-09-25 17:19:11 +0200

    Create base QueryRegistry class

---
+++ src/main/java/org/dddcommunity/sample/infra/action/QueryRegistry.java
@@ -0,0 +1,33 @@
+package org.dddcommunity.sample.infra.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.GenericTypeResolver;
+
+@SuppressWarnings("unchecked")
+final class QueryRegistry extends ActionRegistry<Query, QueryHandler<Query, ?>, QueryHandlerProvider> {
+
+    @Autowired
+    public QueryRegistry(ApplicationContext applicationContext) {
+        super(applicationContext);
+    }
+
+    @Override
+    protected String[] getBeanNames(ApplicationContext applicationContext) {
+        return applicationContext.getBeanNamesForType(QueryHandler.class);
+    }
+
+    @Override
+    protected QueryHandlerProvider getHandlerProvider(ApplicationContext applicationContext, Class<QueryHandler<Query, ?>> queryHandlerClass) {
+        return new QueryHandlerProvider(applicationContext, queryHandlerClass);
+    }
+
+    protected Class<?>[] getGenerics(Class<QueryHandler<Query, ?>> queryHandlerClass) {
+        return GenericTypeResolver.resolveTypeArguments(queryHandlerClass, QueryHandler.class);
+    }
+
+    @Override
+    protected Class<QueryHandler<Query, ?>> getHandlerClass(ApplicationContext applicationContext, String name) {
+        return (Class<QueryHandler<Query, ?>>) applicationContext.getBean(name);
+    }
+}
