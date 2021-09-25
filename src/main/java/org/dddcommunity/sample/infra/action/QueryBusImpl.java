0cccd25dcaab8adc41908d475772626178f7e61d
Joabe Costa <joabetc@hotmail.com>
2021-09-25 17:24:30 +0200

    Implement QueryBus

---
+++ src/main/java/org/dddcommunity/sample/infra/action/QueryBusImpl.java
@@ -0,0 +1,18 @@
+package org.dddcommunity.sample.infra.action;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("unchecked")
+@RequiredArgsConstructor
+@Component
+public final class QueryBusImpl implements QueryBus {
+
+    private final QueryRegistry queryRegistry;
+
+    @Override
+    public <R> R execute(Query action) {
+        var queryHandler = queryRegistry.get(action.getClass());
+        return (R) queryHandler.handle(action);
+    }
+}
